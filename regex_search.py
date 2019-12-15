907444b892c92d808dcffa1f00339b1db9cdfbaa
B10ta <58633974+b10ta@users.noreply.github.com>
2019-12-15 21:23:01 +0100

    cleanup

--- regex_search.py
+++ regex_search.py
@@ -6,16 +6,12 @@ os.system('clear')
 
 print('Working Directory is %s ' % os.getcwd())
 
-def find_regex():
-    for files in os.listdir('./'):
-        if files.endswith(".txt"):
-            text = open(files , 'r' , encoding = 'UTF-8')
-            contents = text.read()
-            matches  = findall(regex_user , contents)
-            if len(contents) > 0:
-                print('%s was found  on "%s"' % (matches , files ) )
-            else:
-                print('no result was found in this Directory ')
-
-
-print(find_regex())
+for files in os.listdir('./'):
+   if files.endswith(".txt"):
+      text = open(files , 'r' , encoding = 'UTF-8')
+      contents = text.read()
+      matches  = findall(regex_user , contents)
+      if len(contents) > 0:
+           print('%s was found  on "%s"' % (matches , files ) )
+      else:
+           print('no result was found in this Directory ')
