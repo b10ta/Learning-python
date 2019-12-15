2166a682070227f46bcd528c37e87b411262afd2
root <root@localhost.localdomain>
2019-12-15 20:29:42 +0100

    learning python

---
+++ regex_search.py
@@ -0,0 +1,22 @@
+import re
+import os
+
+regex_user = input('Enter a regular expression you would  ')
+
+os.system('clear')
+
+print('Working Directory is %s ' % os.getcwd())
+
+def find_regex():
+    for files in os.listdir('./'):
+        if files.endswith(".txt"):
+            text = open(files , 'r' , encoding = 'UTF-8')
+            contents = text.read()
+            matches  = re.findall(regex_user , contents)
+            if len(contents) > 0:
+                print('%s was found  on "%s"' % (matches , files ) )
+            else:
+                ('no result was found in this Directory ')
+
+
+print(find_regex())
