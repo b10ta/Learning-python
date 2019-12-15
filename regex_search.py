5527812bf0bda1e51b2dbf581b1ea4fba99b54ee
adam2692000 <ademcoder@gmail.com>
2019-12-15 21:59:55 +0100

    removed unnecessary comments

--- regex_search.py
+++ regex_search.py
@@ -8,10 +8,9 @@ print('Working Directory is %s ' % getcwd())
 for files in listdir('./'):
     if files.endswith(".txt"):
         text = open(files, 'r', encoding='UTF-8')
-        contents = text.read()  # contents is the file content, it's length is always >0
+        contents = text.read()
         matches = findall(regex_user, contents)
         if len(contents) > 0:
-            # hey? whats that?, this is always false
             print('%s was found  on "%s"' % (matches, files))
         else:
             print('no result was found in this Directory ')
