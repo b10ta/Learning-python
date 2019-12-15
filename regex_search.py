2fb75eea4e9802e6f2975f12c327a4b831d5655e
adam2692000 <ademcoder@gmail.com>
2019-12-15 21:45:24 +0100

    fixed spacing

--- regex_search.py
+++ regex_search.py
@@ -1,16 +1,17 @@
 from re import findall
-from os import system , listdir , getcwd 
+from os import system, listdir, getcwd
 
 regex_user = input('Enter a regular expression you would:  ')
 
 print('Working Directory is %s ' % getcwd())
 
 for files in listdir('./'):
-   if files.endswith(".txt"):
-      text = open(files , 'r' , encoding = 'UTF-8')
-      contents = text.read()
-      matches  = findall(regex_user , contents)
-      if len(contents) > 0:
-           print('%s was found  on "%s"' % (matches , files ) )
-      else:
-           print('no result was found in this Directory ')
+    if files.endswith(".txt"):
+        text = open(files, 'r', encoding='UTF-8')
+        contents = text.read()  # contents is the file content, it's length is always >0
+        matches = findall(regex_user, contents)
+        if len(contents) > 0:
+            # hey? whats that?, this is always false
+            print('%s was found  on "%s"' % (matches, files))
+        else:
+            print('no result was found in this Directory ')
